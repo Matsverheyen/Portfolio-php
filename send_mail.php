6877e4a25744d30ad7e4f4d3e3ee3d792449a071
Mats <99052518@mydavinci.nl>
2019-03-13 12:39:37 +0100

    error reporting

--- send_mail.php
+++ send_mail.php
@@ -1,4 +1,9 @@
 <?php
+
+ni_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require 'vendor/autoload.php';
 require_once('recaptchalib.php');
   $privatekey = "6LecXpcUAAAAABJkStkFCS14oeALTVPPNaTmjuZF";
