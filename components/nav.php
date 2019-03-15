6e0a6a7593b19238acb23bcadb48ef4234cfe2b1
Mats <99052518@mydavinci.nl>
2019-03-15 09:02:26 +0100

    fixed navbar responsive

--- components/nav.php
+++ components/nav.php
@@ -1,16 +1,16 @@
 <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
   <ul class="navbar-nav">
     <li class="nav-item active">
-      <a class="nav-link" href="#">Active</a>
+      <a class="nav-link" href="#">Mats Verheyen</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#">Link</a>
+      <a class="nav-link" href="index.php">Home</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#">Link</a>
+      <a class="nav-link" href="contact.php">Contact</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link disabled" href="#">Disabled</a>
+      <a class="nav-link" href="gallery.php"">Gallery</a>
     </li>
   </ul>
 </nav>
