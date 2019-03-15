d46fd467c6a45f8341c385490fc25c49a7a7c5a1
Mats <99052518@mydavinci.nl>
2019-03-15 08:48:57 +0100

    fixed sizing

--- components/footer.php
+++ components/footer.php
@@ -1,3 +1,3 @@
-<div class="fixed-bottom footer-copyright text-center py-3">© 2019 Copyright:
+<div class="footer-copyright text-center py-3">© 2019 Copyright:
   <a href="#"> Mats Verheyen</a>
 </div>
