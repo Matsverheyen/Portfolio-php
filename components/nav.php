574d89271c1ff6d99ff771e6b392164ac991b738
Mats <99052518@mydavinci.nl>
2019-03-15 08:54:39 +0100

    burger

--- components/nav.php
+++ components/nav.php
@@ -1,10 +1,10 @@
 <nav class="sticky-top navbar-dark bg-primary navbar navbar-expand-lg navbar-expand-sm navbar-expand-mg navbar-expand-xl">
-  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navburger" aria-controls="navburger" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <a class="navbar-brand" href="#">Mats Verheyen</a>
 
-  <div class="collapse navbar-collapse">
+  <div class="collapse navbar-collapse" id="navburger">
     <ul class="navbar-nav mr-auto mt-2 mt-md-0">
       <li class="nav-item">
         <a class="nav-link" href="index.php">Home</a>
