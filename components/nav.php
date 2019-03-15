22d548364f79b4eece41cc2aefe32dfa36ca072d
Mats <99052518@mydavinci.nl>
2019-03-15 08:56:40 +0100

    changed nav

--- components/nav.php
+++ components/nav.php
@@ -1,19 +1,20 @@
-<nav class="sticky-top navbar-dark bg-primary navbar navbar-expand-lg navbar-expand-sm navbar-expand-mg navbar-expand-xl">
-  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navburger" aria-controls="navburger" aria-expanded="false" aria-label="Toggle navigation">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Mats Verheyen</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <a class="navbar-brand" href="#">Mats Verheyen</a>
 
-  <div class="collapse navbar-collapse" id="navburger">
-    <ul class="navbar-nav mr-auto mt-2 mt-md-0">
-      <li class="nav-item">
-        <a class="nav-link" href="index.php">Home</a>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="contact.php">Contact</a>
       </li>
       <li class="nav-item">
-      <a class="nav-link" href="gallery.php">Gallery</a></li>
+        <a class="nav-link" href="gallery.php">Gallery</a>
+      </li>
     </ul>
   </div>
 </nav>
